22d00edcc616d117ce373826f884a2a12be096c9
Philip Welch <Info@opendoorlogistics.com>
2014-10-28 13:31:34 +0000

    added dropdown list for territory painter

--- com.opendoorlogistics.studio/src/com/opendoorlogistics/studio/components/map/InteractiveMapPanel.java
+++ com.opendoorlogistics.studio/src/com/opendoorlogistics/studio/components/map/InteractiveMapPanel.java
@@ -17,6 +17,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Callable;
 
 import javax.swing.AbstractAction;
 import javax.swing.Action;
@@ -46,14 +47,17 @@ import com.opendoorlogistics.core.tables.ODLDatastoreUndoable;
 import com.opendoorlogistics.core.tables.decorators.datastores.UndoRedoDecorator;
 import com.opendoorlogistics.core.tables.utils.TableUtils;
 import com.opendoorlogistics.core.utils.Colours;
+import com.opendoorlogistics.core.utils.Pair;
 import com.opendoorlogistics.core.utils.strings.Strings;
 import com.opendoorlogistics.core.utils.ui.LayoutUtils;
 import com.opendoorlogistics.core.utils.ui.ShowPanel;
+import com.opendoorlogistics.core.utils.ui.VerticalLayoutPanel;
 import com.opendoorlogistics.studio.GlobalMapSelectedRowsManager;
 import com.opendoorlogistics.studio.components.map.InteractiveMapControl.OnClickPosition;
 import com.opendoorlogistics.studio.components.map.InteractiveMapControl.OnFillListener;
 import com.opendoorlogistics.studio.components.map.ModalMouseListener.MouseMode;
 import com.opendoorlogistics.studio.components.map.SelectionPanel.SelectionChangedListener;
+import com.opendoorlogistics.studio.controls.DynamicComboBox;
 import com.opendoorlogistics.studio.panels.FieldSelectorPanel;
 import com.opendoorlogistics.utils.ui.SimpleAction;
 
@@ -65,7 +69,8 @@ public class InteractiveMapPanel extends ReadOnlyMapPanel implements MapSelectio
 	private final TIntHashSet availableTableIds = new TIntHashSet();
 	private JCheckBox showSelectionList;
 	private final HideableSplitPanel mapSelListSplitter;
-	
+	private final SuggestedFillValuesManager fillSuggestedValues = new SuggestedFillValuesManager();
+
 	private class HideableSplitPanel extends JSplitPane {
 		private boolean isOpen = false;
 		private double lastOpenDividerLocation;
@@ -95,7 +100,7 @@ public class InteractiveMapPanel extends ReadOnlyMapPanel implements MapSelectio
 			if (this.isOpen != isOpen && isOpen) {
 				newProportion = lastOpenDividerLocation;
 			} else if (isOpen == false) {
-				newProportion =getOrientation() == JSplitPane.HORIZONTAL_SPLIT ?0.01: 0.99;
+				newProportion = getOrientation() == JSplitPane.HORIZONTAL_SPLIT ? 0.01 : 0.99;
 			}
 
 			if (newProportion != -1) {
@@ -106,10 +111,10 @@ public class InteractiveMapPanel extends ReadOnlyMapPanel implements MapSelectio
 					if (getOrientation() == JSplitPane.HORIZONTAL_SPLIT) {
 						// get new map viewer width
 						int oldMapWidth = interactive.getWidth();
-						int newMapWidth = (int) Math.round(space *  (1.0-newProportion));
+						int newMapWidth = (int) Math.round(space * (1.0 - newProportion));
 						int halfNewMapWidth = (int) Math.round(newMapWidth / 2.0);
 						int newCentreX = oldMapWidth - halfNewMapWidth;
-						
+
 						// get current geographic position at what would be the new on-screen centre
 						LatLong pos = converter.getLongLat(newCentreX, (int) Math.round(interactive.getHeight() / 2.0));
 
@@ -121,8 +126,7 @@ public class InteractiveMapPanel extends ReadOnlyMapPanel implements MapSelectio
 						int newMapHeight = (int) Math.round(space * newProportion);
 
 						// get current geographic position at what would be the new on-screen centre
-						LatLong pos =converter.getLongLat((int) Math.round(interactive.getWidth() / 2.0),
-								(int) Math.round(newMapHeight / 2.0) - 2);
+						LatLong pos = converter.getLongLat((int) Math.round(interactive.getWidth() / 2.0), (int) Math.round(newMapHeight / 2.0) - 2);
 
 						// set the map to be centred on this
 						interactive.setCenterPosition(new GeoPosition(pos.getLatitude(), interactive.getCenterPosition().getLongitude()));
@@ -139,12 +143,10 @@ public class InteractiveMapPanel extends ReadOnlyMapPanel implements MapSelectio
 	private void updateAppearance() {
 		fieldSelectorPanel.setVisible(interactive.getMouseMode() == MouseMode.FILL);
 	}
-	
 
-	
-	public InteractiveMapPanel(MapConfig config,List<? extends DrawableObject> pnts, final ODLDatastoreUndoable<ODLTableAlterable> globalDs, GlobalMapSelectedRowsManager gsm) {
-		super(new InteractiveMapControl(config,new SelectionPanel(globalDs), gsm),false);
-				
+	public InteractiveMapPanel(MapConfig config, List<? extends DrawableObject> pnts, final ODLDatastoreUndoable<ODLTableAlterable> globalDs, GlobalMapSelectedRowsManager gsm) {
+		super(new InteractiveMapControl(config, new SelectionPanel(globalDs), gsm), false);
+
 		this.globalDs = globalDs;
 		interactive = (InteractiveMapControl) map;
 
@@ -153,7 +155,7 @@ public class InteractiveMapPanel extends ReadOnlyMapPanel implements MapSelectio
 
 		fieldSelectorPanel = new FillFieldPanel(globalDs);
 		fillSplitPanel = new HideableSplitPanel(JSplitPane.HORIZONTAL_SPLIT, fieldSelectorPanel, interactive, 0.25);
-		
+
 		// call set points *after* initialising the fill field selector panel as it call it..
 		setDrawables(pnts);
 
@@ -175,102 +177,78 @@ public class InteractiveMapPanel extends ReadOnlyMapPanel implements MapSelectio
 
 			@Override
 			public void onFill(TLongArrayList selected) {
-				String[] field = fieldSelectorPanel.getField();
-				String value = fieldSelectorPanel.getFillValue();
-
-				// find table...
-				ODLTable table = null;
-				int col = -1;
-				if (field != null && field.length == 2 && field[0] != null && field[1] != null) {
-					table = TableUtils.findTable(globalDs, field[0], true);
-					if (table != null) {
-						col = TableUtils.findColumnIndx(table, field[1], true);
-					}
-				}
-
-				if (col == -1) {
-					JOptionPane.showMessageDialog(InteractiveMapPanel.this, "No fill column selected.");
-					return;
-				}
-
-				int nbProcessed = 0;
-				int nbSet = 0;
-				globalDs.startTransaction();
-				for (long id : selected.toArray()) {
-					nbProcessed++;
-					ODLTable objTable = globalDs.getTableByImmutableId(TableUtils.getTableId(id));
-					if (objTable == null || objTable.getImmutableId() != table.getImmutableId()) {
-						continue;
-					}
-
-					table.setValueById(value,id, col);
-					nbSet++;
-
-				}
-				globalDs.endTransaction();
-
-				if (nbProcessed > 0 && nbSet == 0) {
-					JOptionPane.showMessageDialog(InteractiveMapPanel.this,
-							"No values were set as none of the selected objects were from the selected table \"" + field[0] + "\".");
-				}
+				processFill(globalDs, selected);
 			}
 		});
-		
+
 		interactive.addOnCreateListener(new OnClickPosition() {
-			
+
 			@Override
 			public void onClickPosition(double latitude, double longitude) {
 				DrawableObjectImpl pnt = new DrawableObjectImpl(latitude, longitude, Color.BLACK, "");
 				long created = createPoint(pnt);
-				if(created!=-1){
-					interactive.setSelected(created);					
+				if (created != -1) {
+					interactive.setSelected(created);
 				}
 			}
 		});
 
 		interactive.addOnMoveObjectListener(new OnClickPosition() {
-			
+
 			@Override
 			public void onClickPosition(double latitude, double longitude) {
 				// get the selected drawable...
 				ArrayList<DrawableObject> pnts = new ArrayList<>();
-				for(DrawableObject point : map.getDrawables()){
-					if(interactive.getSelected().contains(point.getGlobalRowId())){
+				for (DrawableObject point : map.getDrawables()) {
+					if (interactive.getSelected().contains(point.getGlobalRowId())) {
 						pnts.add(point);
 					}
 				}
-	
-				if(pnts.size()==0){
-					JOptionPane.showMessageDialog(InteractiveMapPanel.this,"No point selected.");
+
+				if (pnts.size() == 0) {
+					JOptionPane.showMessageDialog(InteractiveMapPanel.this, "No point selected.");
 					return;
 				}
-				
-				if(pnts.size()>1){
-					JOptionPane.showMessageDialog(InteractiveMapPanel.this,"More than one point selected, cannot set position.");
+
+				if (pnts.size() > 1) {
+					JOptionPane.showMessageDialog(InteractiveMapPanel.this, "More than one point selected, cannot set position.");
 					return;
 				}
-				
-				if(!movePoint(pnts.get(0).getGlobalRowId(), new LatLongImpl(latitude, longitude))){
-					JOptionPane.showMessageDialog(InteractiveMapPanel.this,"Error, could not move point.");					
+
+				if (!movePoint(pnts.get(0).getGlobalRowId(), new LatLongImpl(latitude, longitude))) {
+					JOptionPane.showMessageDialog(InteractiveMapPanel.this, "Error, could not move point.");
 				}
 			}
 		});
-		
+
 		globalDs.addListener(fieldSelectorPanel);
 
 		updateAppearance();
 	}
 
-	protected long createPoint(DrawableObjectImpl newPoint){
+	protected long createPoint(DrawableObjectImpl newPoint) {
 		throw new UnsupportedOperationException();
 	}
 
-	protected boolean movePoint(long globalId, LatLongImpl newPosition){
-		throw new UnsupportedOperationException();		
+	protected boolean movePoint(long globalId, LatLongImpl newPosition) {
+		throw new UnsupportedOperationException();
 	}
-	
+
 	private class FillFieldPanel extends JPanel implements ODLListener {
-		final JTextField text;
+		// final DynamicComboBox<String> text = new DynamicComboBox<String>("",false,true) {
+		//
+		// @Override
+		// protected List<String> getAvailableItems() {
+		// Pair<ODLTable, Integer> tableCol = getSelectedFillTableColumn();
+		// if(tableCol==null){
+		// return new ArrayList<>();
+		// }
+		// return fillSuggestedValues.getSuggestions( tableCol.getFirst(), tableCol.getSecond(), interactive.getDrawables());
+		// }
+		// };
+
+		// final JTextField text = new JTextField();
+		final DynamicComboBox<String> text;
 		final FieldSelectorPanel fieldSelector;
 
 		FillFieldPanel(ODLDatastore<? extends ODLTable> globalDs) {
@@ -279,12 +257,26 @@ public class InteractiveMapPanel extends ReadOnlyMapPanel implements MapSelectio
 			fieldSelector = new FieldSelectorPanel(globalDs);
 			add(fieldSelector, BorderLayout.CENTER);
 
-			text = new JTextField();
-			add(LayoutUtils.createVerticalBoxLayout(new JLabel("Set fill value:"), text), BorderLayout.SOUTH);
+			text = new DynamicComboBox<String>("", true, true) {
+
+				@Override
+				protected List<String> getAvailableItems() {
+					Pair<ODLTable, Integer> tableCol = getSelectedFillTableColumn();
+					if (tableCol == null) {
+						return new ArrayList<>();
+					}
+					return fillSuggestedValues.getSuggestions(tableCol.getFirst(), tableCol.getSecond(), interactive.getDrawables());
+				}
+			};
+			
+			VerticalLayoutPanel vlp = new VerticalLayoutPanel();
+			vlp.addLine(new JLabel("Set fill value:"));
+			vlp.addLine(text);
+			add(vlp, BorderLayout.SOUTH);
 		}
 
 		String getFillValue() {
-			return text.getText();
+			return text.getValue();
 		}
 
 		String[] getField() {
@@ -299,7 +291,7 @@ public class InteractiveMapPanel extends ReadOnlyMapPanel implements MapSelectio
 
 		@Override
 		public void datastoreStructureChanged() {
-			fieldSelector.update(globalDs,availableTableIds);
+			fieldSelector.update(globalDs, availableTableIds);
 		}
 
 		@Override
@@ -328,55 +320,54 @@ public class InteractiveMapPanel extends ReadOnlyMapPanel implements MapSelectio
 
 	public static InteractiveMapPanel createWithDummyData() {
 		ODLDatastoreAlterable<ODLTableAlterable> ds = MapUtils.createExampleDatastore(1000);
-		UndoRedoDecorator<ODLTableAlterable>undoRedo = new UndoRedoDecorator<>(ODLTableAlterable.class, ds);
+		UndoRedoDecorator<ODLTableAlterable> undoRedo = new UndoRedoDecorator<>(ODLTableAlterable.class, ds);
 		List<DrawableObjectImpl> points = DrawableObjectImpl.getBeanMapping().getTableMapping(0).readObjectsFromTable(ds.getTableAt(0));
-		InteractiveMapPanel panel = new InteractiveMapPanel(new MapConfig(), points, undoRedo,null);
+		InteractiveMapPanel panel = new InteractiveMapPanel(new MapConfig(), points, undoRedo, null);
 		return panel;
-		
-//		// lines test
-//	//	ArrayList<DrawableObject> drawables = new ArrayList<>();
-//		List<Pair<String, LatLong>> places = ExampleData.getUKPlaces();
-//		LatLong [] latLongs = new LatLong[places.size()];
-//		for(int i =0 ; i<latLongs.length ;i++){
-//			latLongs[i] = places.get(i).getSecond();
-//		}
-//		ODLSimpleGeom simpleGeom = new ODLSimpleGeom(SimpleGeomType.POLYGON, latLongs);
-//		ODLGeom geom = new ODLGeom(new ODLSimpleGeom[]{simpleGeom});
-//		List<ODLGeom> geoms = new ArrayList<>();
-//		geoms.add(geom);
-//		return createShowingGeoms(geoms);
+
+		// // lines test
+		// // ArrayList<DrawableObject> drawables = new ArrayList<>();
+		// List<Pair<String, LatLong>> places = ExampleData.getUKPlaces();
+		// LatLong [] latLongs = new LatLong[places.size()];
+		// for(int i =0 ; i<latLongs.length ;i++){
+		// latLongs[i] = places.get(i).getSecond();
+		// }
+		// ODLSimpleGeom simpleGeom = new ODLSimpleGeom(SimpleGeomType.POLYGON, latLongs);
+		// ODLGeom geom = new ODLGeom(new ODLSimpleGeom[]{simpleGeom});
+		// List<ODLGeom> geoms = new ArrayList<>();
+		// geoms.add(geom);
+		// return createShowingGeoms(geoms);
 	}
 
 	public static InteractiveMapPanel createShowingGeoms(List<ODLGeomImpl> geoms) {
 		ArrayList<DrawableObjectImpl> list = new ArrayList<>();
-		for(int i =0 ; i<geoms.size();i++){
+		for (int i = 0; i < geoms.size(); i++) {
 			ODLGeomImpl geom2 = geoms.get(i);
 			DrawableObjectImpl drawable = new DrawableObjectImpl();
 			drawable.setGeometry(geom2);
 			drawable.setColour(Colours.getRandomColour(i));
-			list.add(drawable);			
+			list.add(drawable);
 		}
-		return createShowing(list,true);
+		return createShowing(list, true);
 	}
 
 	public static InteractiveMapPanel createShowing(List<DrawableObjectImpl> list, boolean setGlobalIdInList) {
-		
+
 		// create a dummy datastore
 		ODLDatastoreAlterable<ODLTableAlterable> ds = MapUtils.createDatastore(list, setGlobalIdInList);
-		UndoRedoDecorator<ODLTableAlterable>undoRedo = new UndoRedoDecorator<>(ODLTableAlterable.class, ds);
-		
-		InteractiveMapPanel panel = new InteractiveMapPanel(new MapConfig(), list, undoRedo,null);
+		UndoRedoDecorator<ODLTableAlterable> undoRedo = new UndoRedoDecorator<>(ODLTableAlterable.class, ds);
+
+		InteractiveMapPanel panel = new InteractiveMapPanel(new MapConfig(), list, undoRedo, null);
 		return panel;
 	}
-	
-	
+
 	@Override
 	protected JToolBar createToolbar() {
 		JToolBar ret = super.createToolbar();
 		ret.addSeparator();
 		showSelectionList = new JCheckBox("Sel list", true);
 		showSelectionList.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				updateSelListSplitter();
@@ -385,11 +376,11 @@ public class InteractiveMapPanel extends ReadOnlyMapPanel implements MapSelectio
 		ret.add(showSelectionList);
 		return ret;
 	}
-	
+
 	protected List<Action> createActions() {
 		List<Action> ret = super.createActions();
 		ret.add(null);
-		
+
 		// create action to enter each of the mouse modes
 		for (final MouseMode mode : MouseMode.values()) {
 			AbstractAction action = new AbstractAction(Strings.convertEnumToDisplayFriendly(mode.toString()), mode.getButtonImageIcon()) {
@@ -420,41 +411,41 @@ public class InteractiveMapPanel extends ReadOnlyMapPanel implements MapSelectio
 				}
 
 				TableUtils.deleteByGlobalId(globalDs, true, sel.toArray());
-//				globalDs.startTransaction();
-//				sel.forEach(new TLongProcedure() {
-//
-//					@Override
-//					public boolean execute(long value) {
-//						ODLTable table = globalDs.getTableByImmutableId(Utils.getTableId(value));
-//						if (table != null) {
-//							int row = table.getRowIndexByLocalId(Utils.getLocalRowId(value));
-//							if (row != -1) {
-//								table.deleteRow(row);
-//							}
-//						}
-//						return true;
-//					}
-//				});
-//				globalDs.endTransaction();
+				// globalDs.startTransaction();
+				// sel.forEach(new TLongProcedure() {
+				//
+				// @Override
+				// public boolean execute(long value) {
+				// ODLTable table = globalDs.getTableByImmutableId(Utils.getTableId(value));
+				// if (table != null) {
+				// int row = table.getRowIndexByLocalId(Utils.getLocalRowId(value));
+				// if (row != -1) {
+				// table.deleteRow(row);
+				// }
+				// }
+				// return true;
+				// }
+				// });
+				// globalDs.endTransaction();
 			}
 		});
 		return ret;
 	}
 
 	@Override
-	public void setDrawables(Iterable<? extends DrawableObject> pnts){
+	public void setDrawables(Iterable<? extends DrawableObject> pnts) {
 		// parse to get all distinct table ids
 		availableTableIds.clear();
-		for(DrawableObject pnt:pnts){
+		for (DrawableObject pnt : pnts) {
 			long id = pnt.getGlobalRowId();
-			if(id!=-1){
+			if (id != -1) {
 				availableTableIds.add(TableUtils.getTableId(id));
 			}
 		}
-	
+
 		// call the datastore structure changed event so it re-reads the avaiable tables
 		fieldSelectorPanel.datastoreStructureChanged();
-		
+
 		super.setDrawables(pnts);
 	}
 
@@ -464,11 +455,73 @@ public class InteractiveMapPanel extends ReadOnlyMapPanel implements MapSelectio
 	}
 
 	private void updateSelListSplitter() {
-		boolean enabled = showSelectionList!=null?showSelectionList.isSelected():true;
+		boolean enabled = showSelectionList != null ? showSelectionList.isSelected() : true;
 		mapSelListSplitter.setOpen(enabled && interactive.getSelectionPanel().getSelectedRowsCount() > 0);
 	}
 
-	protected String getToolTipText(TLongArrayList selectedIds){
+	protected String getToolTipText(TLongArrayList selectedIds) {
 		return null;
+	}
+
+	private Pair<ODLTable, Integer> getSelectedFillTableColumn() {
+		if (fieldSelectorPanel == null) {
+			return null;
+		}
+		String[] tableFieldPair = fieldSelectorPanel.getField();
+		ODLTable table = null;
+		int col = -1;
+		if (tableFieldPair != null && tableFieldPair.length == 2 && tableFieldPair[0] != null && tableFieldPair[1] != null) {
+			table = TableUtils.findTable(globalDs, tableFieldPair[0], true);
+			if (table != null) {
+				col = TableUtils.findColumnIndx(table, tableFieldPair[1], true);
+			}
+		}
+
+		if (col != -1) {
+			return new Pair<ODLTable, Integer>(table, col);
+		}
+		return null;
+	}
+
+	private void processFill(final ODLDatastoreUndoable<ODLTableAlterable> globalDs, final TLongArrayList selected) {
+		final Pair<ODLTable, Integer> pair = getSelectedFillTableColumn();
+
+		if (pair == null) {
+			JOptionPane.showMessageDialog(InteractiveMapPanel.this, "No fill column selected.");
+			return;
+		}
+
+		// get the fill value and log this fill to the suggested values manager
+		final String value = fieldSelectorPanel.getFillValue();
+		fillSuggestedValues.addFill(pair.getFirst().getName(), pair.getFirst().getColumnName(pair.getSecond()), value);
+
+		class Counter {
+			int nbProcessed = 0;
+			int nbSet = 0;
+		}
+		final Counter counter = new Counter();
+
+		TableUtils.runTransaction(globalDs, new Callable<Boolean>() {
+
+			@Override
+			public Boolean call() throws Exception {
+				for (long id : selected.toArray()) {
+					counter.nbProcessed++;
+					ODLTable objTable = globalDs.getTableByImmutableId(TableUtils.getTableId(id));
+					if (objTable == null || objTable.getImmutableId() != pair.getFirst().getImmutableId()) {
+						continue;
+					}
+
+					pair.getFirst().setValueById(value, id, pair.getSecond());
+					counter.nbSet++;
+
+				}
+				return true;
+			}
+		});
+
+		if (counter.nbProcessed > 0 && counter.nbSet == 0) {
+			JOptionPane.showMessageDialog(InteractiveMapPanel.this, "No values were set as none of the selected objects were from the selected table.");
+		}
 	}
 }
---
+++ com.opendoorlogistics.studio/src/com/opendoorlogistics/studio/components/map/SuggestedFillValuesManager.java
@@ -0,0 +1,121 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Open Door Logistics (www.opendoorlogistics.com)
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser Public License 3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl.html
+ * 
+ ******************************************************************************/
+package com.opendoorlogistics.studio.components.map;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+
+import com.opendoorlogistics.api.tables.ODLColumnType;
+import com.opendoorlogistics.api.tables.ODLTableReadOnly;
+import com.opendoorlogistics.core.gis.map.data.DrawableObject;
+import com.opendoorlogistics.core.tables.ColumnValueProcessor;
+import com.opendoorlogistics.core.tables.utils.TableUtils;
+import com.opendoorlogistics.core.utils.Pair;
+import com.opendoorlogistics.core.utils.strings.StandardisedStringTreeMap;
+import com.opendoorlogistics.core.utils.strings.Strings;
+
+public class SuggestedFillValuesManager {
+	private HashMap<Pair<String,String>, StandardisedStringTreeMap<Long>> maps = new HashMap<>();
+	private long fillNb;
+	
+	void addFill(String table, String field, String value){
+		if(Strings.isEmptyWhenStandardised(value)){
+			return;
+		}
+		
+		StandardisedStringTreeMap<Long> fldMap = getMap(table, field);	
+		fldMap.put(value, fillNb++);
+	}
+
+	private StandardisedStringTreeMap<Long> getMap(String table, String field) {
+		
+		Pair<String,String> id = new Pair<String, String>(Strings.std(table), Strings.std(field));
+		StandardisedStringTreeMap<Long> fldMap = maps.get(id);
+		if(fldMap==null){
+			fldMap = new StandardisedStringTreeMap<>();
+			maps.put(id, fldMap);
+		}
+		return fldMap;
+	}
+	
+	List<String> getSuggestions( ODLTableReadOnly table, int col, Iterable<? extends DrawableObject> drawables){
+		
+		// get all possible values...
+		StandardisedStringTreeMap<String> canonical = new StandardisedStringTreeMap<>();
+		for(DrawableObject o:drawables){
+			if(o.getSelectable()!=0 && TableUtils.getTableId(o.getGlobalRowId())==table.getImmutableId()){
+				Object value = table.getValueById(o.getGlobalRowId(), col);
+				if(value!=null){
+					String s = (String)ColumnValueProcessor.convertToMe(ODLColumnType.STRING, value);
+					if(!canonical.containsKey(s)){
+						canonical.put(s, s);						
+					}
+				}
+			//	canonical.put(id, o)
+			}
+		}
+		
+		final StandardisedStringTreeMap<Long> lastUsedMap = getMap(table.getName(), table.getColumnName(col));
+		
+		// get in array list and sort by last used time (so largest first)
+		List<String> list = new ArrayList<>(canonical.values());
+		Collections.sort(list, new Comparator<String>() {
+
+			@Override
+			public int compare(String o1, String o2) {
+				Long fill1 = replaceNull(lastUsedMap.get(o1));
+				Long fill2 = replaceNull(lastUsedMap.get(o2));
+				return fill2.compareTo(fill1);
+			}
+			
+			private Long replaceNull(Long l){
+				if(l!=null){
+					return l;
+				}
+				return Long.MIN_VALUE;
+			}
+		});
+	
+//		// show only the top ones...
+//		int n =1000;
+//		if(list.size()>n){
+//			list = list.subList(0, n);
+//		}
+		return list;
+	}
+	
+//	boolean wordTyped(String typedWord) {
+//
+//		if (typedWord.isEmpty()) {
+//			return false;
+//		}
+//		// System.out.println("Typed word: " + typedWord);
+//
+//		boolean suggestionAdded = false;
+//
+//		for (String word : dictionary) {// get words in the dictionary which we added
+//			boolean fullymatches = true;
+//			for (int i = 0; i < typedWord.length(); i++) {// each string in the word
+//				if (!typedWord.toLowerCase().startsWith(String.valueOf(word.toLowerCase().charAt(i)), i)) {// check for match
+//					fullymatches = false;
+//					break;
+//				}
+//			}
+//			if (fullymatches) {
+//				addWordToSuggestions(word);
+//				suggestionAdded = true;
+//			}
+//		}
+//		return suggestionAdded;
+//	}
+	
+}
