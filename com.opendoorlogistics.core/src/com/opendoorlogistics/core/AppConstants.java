7f717c716db8f1360d6ba091ec228fdf66ba4c75
Philip Welch <Info@opendoorlogistics.com>
2014-10-29 13:42:02 +0000

    version change

--- com.opendoorlogistics.core/src/com/opendoorlogistics/core/AppConstants.java
+++ com.opendoorlogistics.core/src/com/opendoorlogistics/core/AppConstants.java
@@ -20,7 +20,7 @@ final public class AppConstants {
 	
 	public static final int APP_VERSION_MINOR = 1;
 
-	public static final int APP_VERSION_REVISION = 6;
+	public static final int APP_VERSION_REVISION = 7;
 	
 	public static final String SHAPEFILES_DIRECTORY = "data" + File.separator + "shapefiles" + File.separator;
 
