2a755e8bae9c7e8954f34e816c7e431e8ac4c70c
Philip Welch <Info@opendoorlogistics.com>
2015-11-11 13:01:46 +0000

    added dummy main method to aid export as fat jar from eclipse

---
+++ com.opendoorlogistics.core/src/com/opendoorlogistics/core/CoreDummyMain.java
@@ -0,0 +1,9 @@
+package com.opendoorlogistics.core;
+
+public class CoreDummyMain {
+
+	public static void main(String[] args) {
+		System.out.println("Dummy main method so we can use Eclipse's functionality to export a fat jar, which requires the jar be runnable.");
+	}
+
+}
